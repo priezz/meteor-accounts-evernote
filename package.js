6847b7f6951cecebbf298a56786af377da27a1c5
Alexandr Priezzhev <a@priezz.net>
2015-07-10 09:35:46 -0400

    0.2.4

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
 	name: 'priezz:accounts-evernote',
 	summary: "Login service for Evernote accounts",
-	version: "0.2.3",
-	//git: "https://github.com/priezz/meteor-accounts-evernote.git"
+	version: "0.2.4",
+	git: "https://github.com/priezz/meteor-accounts-evernote.git"
 });
 
-Npm.depends({
-	 "evernote": "1.25.8"
-	// "url": "1.1.0"
-});
+//Npm.depends({
+//	 "evernote": "1.25.8"
+//	// "url": "1.1.0"
+//});
 
 Package.onUse(function(api) {
 	api.versionsFrom('METEOR@1.0.1')
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
 	api.imply('accounts-base', ['client', 'server']);
 	api.use('accounts-oauth', ['client', 'server']);
 	//api.imply('accounts-oauth', ['client', 'server']);
-	api.use('priezz:evernote@0.2.3', ['client', 'server']);
+	api.use('priezz:evernote@0.3.0', ['client', 'server']);
 
 	api.addFiles('evernote_login_button.css', 'client');
 
